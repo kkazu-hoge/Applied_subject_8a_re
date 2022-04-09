e28f430ae865e34f7a328c1d8f87d077b2cb293a
kkazu-hoge <928kuni@gmail.com>
2022-04-09 06:52:50 +0000

    [finish]app_sub_8a

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,10 +5,26 @@ class ChatsController < ApplicationController
 
 
   def index
-
+    @to_user = User.find(params[:user_id])
+    @from_user = User.find(params[:follower_id])
+    follower = Follower.find_by(user_id: @from_user.id)
+    chat_room_id = Relationship.find_by(follower_id: follower.id, followed_id: @to_user.id).chat_room_id
+    @chat_index = Chat.where(chat_room_id: chat_room_id)
+    @chat_message = Chat.new
   end
 
   def create
+    @chat = Chat.new(chat_message_params)
+    @chat.from_user_id = params[:from_user_id]
+    chat_room_id = Relationship.find_by(follower_id: @chat.from_user_id, followed_id: params[:user_id]).chat_room_id
+    @chat.chat_room_id = chat_room_id
+    @chat.save
+    redirect_to request.referer
+  end
+
+  private
+  def chat_message_params
+    params.require(:chat).permit(:message)
   end
 
 end
