5e6cda4ecab459bba6fcc17082c11a6436edd5a0
kkazu-hoge <928kuni@gmail.com>
2022-04-10 04:35:48 +0000

    [add]error message

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,6 @@
 class Chat < ApplicationRecord
    has_many :relationships, foreign_key: "chat_room_id"
    has_many :chats, foreign_key: "chat_room_id"
+
+  validates :message, length: {maximum: 140}
 end
